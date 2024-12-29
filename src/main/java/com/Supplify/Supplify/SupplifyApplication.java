09e1b283cc149e1c87a0d55316157bf998802bb3
lidor <lidor673@gmail.com>
2024-12-29 14:45:25 +0200

    initialization

---
+++ src/main/java/com/Supplify/Supplify/SupplifyApplication.java
@@ -0,0 +1,13 @@
+package com.Supplify.Supplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Supplify/Supplify/SupplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Supplify.Supplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
