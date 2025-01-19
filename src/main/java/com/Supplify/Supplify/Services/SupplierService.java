6e70bc2ee4df0dc62cc49e3bcc772dedacda79f0
Shoham <shoamivgi1234@gmail.com>
2025-01-19 14:59:46 +0200

    s

--- src/main/java/com/Supplify/Supplify/Services/SupplierService.java
+++ src/main/java/com/Supplify/Supplify/Services/SupplierService.java
@@ -1,13 +1,27 @@
 package com.Supplify.Supplify.Services;
-import com.Supplify.Supplify.entities.Supplier;
+
+import com.Supplify.Supplify.entities.BusinessSupplier;
 import com.Supplify.Supplify.repositories.SupplierRepo;
+import com.Supplify.Supplify.repositories.BusinessSupplier_Repo;
+import com.Supplify.Supplify.repositories.BusinessRepo;
+import com.Supplify.Supplify.entities.Supplier;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.List;
+
 import java.util.Optional;
+import java.util.Set;
 
-@AllArgsConstructor
 @Service
+@AllArgsConstructor
 public class SupplierService {
+    private final SupplierRepo supplierRepo;
+    private final BusinessSupplier_Repo businessSupplierRepo;  // Added this repository
 
+    public void createSupplier(Supplier supplier, Integer businessId) {
+        // First save the supplier
+        Supplier savedSupplier = supplierRepo.save(supplier);
+        // Create and save the business-supplier relationship
+        BusinessSupplier businessSupplier = new BusinessSupplier(businessId, savedSupplier.getSupplierId());
+        businessSupplierRepo.save(businessSupplier);
+    }
 }
--- src/main/java/com/Supplify/Supplify/entities/BusinessSupplier.java
+++ src/main/java/com/Supplify/Supplify/entities/BusinessSupplier.java
@@ -16,26 +16,13 @@ public class BusinessSupplier {
     @EmbeddedId
     private BusinessSupplierId id;
 
-    // Additional fields if necessary
-    @ManyToOne
-    @JoinColumn(name = "business_id", insertable = false, updatable = false)
-    private Business business;
-
-    @ManyToOne
-    @JoinColumn(name = "supplier_id", insertable = false, updatable = false)
-    private Supplier supplier;
-
-    // Composite key class
     @Embeddable
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Getter
     @Setter
-    @NoArgsConstructor
+    @EqualsAndHashCode
     public static class BusinessSupplierId implements Serializable {
-        public BusinessSupplierId(int businessId, int supplierId) {
-            this.businessId = businessId;
-            this.supplierId = supplierId;
-        }
-
         @Column(name = "business_id")
         private int businessId;
 
@@ -47,4 +34,17 @@ public class BusinessSupplier {
     public BusinessSupplier(int businessId, int supplierId) {
         this.id = new BusinessSupplierId(businessId, supplierId);
     }
+
+    // Optional: Add these if you need to map to the actual Business and Supplier entities
+    /*
+    @ManyToOne
+    @MapsId("businessId")
+    @JoinColumn(name = "business_id")
+    private Business business;
+
+    @ManyToOne
+    @MapsId("supplierId")
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+    */
 }
