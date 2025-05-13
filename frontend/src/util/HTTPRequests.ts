2e4840f1cb49893935f93cd2adea81cba52c65e5
Talya-Perets <talyape123@gmail.com>
2025-05-13 15:45:09 +0300

    connect Amazon

--- frontend/src/util/HTTPRequests.ts
+++ frontend/src/util/HTTPRequests.ts
@@ -41,31 +41,6 @@ export const doPostAddProduct = async (url: string, data: any, params?: any) => 
     }
     
   };
-  // Function to get all users for a business
-export const doGetBusinessUsers = async (businessId: number) => {
-  try {
-    const url = `${globals.USER.getBusinessUsers}/${businessId}`;
-    console.log('Fetching users for business ID:', businessId);
-    
-    const response = await doGet(url);
-    return response;
-  } catch (error) {
-    console.error('Error fetching business users:', error);
-    throw error;
-  }
-};
 
-// Function to delete a user
-export const doDeleteUser = async (userId: number) => {
-  try {
-    const url = `${globals.USER.deleteUser}/${userId}`;
-    console.log('Deleting user with ID:', userId);
-    
-    const response = await axios.delete(url);
-    return response;
-  } catch (error) {
-    console.error('Error deleting user:', error);
-    throw error;
-  }
-};
+
   
